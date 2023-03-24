631396bfe63fb07348b1fb3ffe2b0e8f8f3b1ae2
geonho1943 <thsrjsgh02@naver.com>
2023-03-25 04:36:59 +0900

    boj 알고리즘 수업 - 시간복잡도

---
+++ src/main/java/org/problem/solving/BOJ/Java24262.java
@@ -0,0 +1,13 @@
+package org.problem.solving.BOJ;
+
+import java.util.Scanner;
+
+public class Java24262 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        sc.close();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ src/main/java/org/problem/solving/BOJ/Java24263.java
@@ -0,0 +1,14 @@
+package org.problem.solving.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Java24263 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.println(1);
+    }
+}
